217d2b04d42dbdc902b0f382d2ebf898c600c51a
battlepe <battlepe>
2007-12-31 18:12:39 +0000

    Initial Import

---
+++ RearViewMirror/Updater.cs
@@ -0,0 +1,30 @@
+/**
+ * RearViewMirror - Sumit Khanna 
+ * http://penguindreams.org
+ * 
+ * License: GNU GPLv3 - Free to Distribute so long as any 
+ *   modifications are released for free as well
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RearViewMirror
+{
+    /// <summary>
+    /// Used to connect to penguindreams.org and check for updates
+    /// </summary>
+    public class Updater
+    {
+
+        public readonly static String UPDATE_URL = "http://penguindreams.org/files/";
+
+        public static Boolean checkForUpdates(int rmajor, int rminor)
+        {
+            //TODO: Implement me
+            return false;
+        }
+
+    }
+}
